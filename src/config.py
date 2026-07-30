501a8091b43da6dbb87677c62f00905561d54ef7
Pawan Bhardwaj <ps173702@gmail.com>
2026-07-31 00:23:19 +0530

    Refactor MODEL_PATH to include notebooks directory
    Updated MODEL_PATH to include 'notebooks' directory.

--- src/config.py
+++ src/config.py
@@ -3,7 +3,14 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).resolve().parent.parent
 
 DATA_PATH = ROOT_DIR / "data" / "processed" / "feature_engineered_dataset.csv"
-MODEL_PATH = ROOT_DIR / "models" / "tuned_random_forest_model.pkl"
+
+MODEL_PATH = (
+    ROOT_DIR
+    / "notebooks"
+    / "models"
+    / "tuned_random_forest_model.pkl"
+)
+
 LOG_DIR = ROOT_DIR / "logs"
 ARTIFACT_DIR = ROOT_DIR / "artifacts"
 
